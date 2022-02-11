e0fed8f9fdf877a5f2cfda71d9a1813794a71186
Mike Brennan <mike@livelyvideo.tv>
2022-02-11 14:08:03 -0600

    Daemonize client and server, add configs and ability to control with signals

--- exporter.go
+++ exporter.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
+
+	"github.com/namsral/flag"
 )
+
+const defaultTick = 10 * time.Second
+
+type config struct {
+	directory    string
+	statshosturl string
+	binarytocall string
+	tick         time.Duration
+}
 
 type payloadEntry struct {
 	Filename   string
@@ -19,63 +36,146 @@ type payloadEntry struct {
 
 var fileList []payloadEntry
 
+//Initialization function - sets up the configuration fields
+func (c *config) init(args []string) error {
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+	flags.String(flag.DefaultConfigFlagname, "", "Path to config file")
+
+	var (
+		directory    = flags.String("logs_directory", "/go/src/files/", "Directory to read log files from")
+		tick         = flags.Duration("tick", defaultTick, "Ticking interval")
+		statshosturl = flags.String("server_url", "http://172.17.0.3:8080/", "Url to use for posts to stats host")
+		binarytocall = flags.String("binary", "/bin/decgrep", "Command to call to read binary log")
+	)
+
+	if err := flags.Parse(args[1:]); err != nil {
+		fmt.Println("Error:")
+		fmt.Println(err)
+		return err
+	}
+
+	c.tick = *tick
+	c.directory = *directory
+	c.statshosturl = *statshosturl
+	c.binarytocall = *binarytocall
+
+	return nil
+}
+
 func main() {
-	// load directory to find files to run the wrapper against.  Using the directory and filenames - generate the metadata to send to
-	// server, Post the metadata in a specific way - and then the metadata is needed to be used there to re-create the file in a
-	// structured manner.
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
 
-	directoryPath := "/go/src/files/"
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGHUP)
 
-	payLoad := generatePayload(directoryPath, fileList)
-	// fmt.Println("Reading payload:")
-	for _, entry := range payLoad {
-		call("http://172.17.0.3:8080/", "POST", entry)
-		fmt.Printf("%s:  - %s\n", entry.Filename, strconv.Itoa(entry.Timestamp))
+	c := &config{}
+
+	defer func() {
+		signal.Stop(signalChan)
+		cancel()
+	}()
+
+	go func() {
+		for {
+			select {
+			case s := <-signalChan:
+				switch s {
+				case syscall.SIGHUP:
+					c.init(os.Args)
+				case os.Interrupt:
+					cancel()
+					os.Exit(1)
+				}
+			case <-ctx.Done():
+				log.Printf("Done.")
+				os.Exit(1)
+			}
+		}
+	}()
+
+	if err := run(ctx, c, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
 	}
 }
 
-func generatePayload(directoryPath string, payload []payloadEntry) []payloadEntry {
+func run(ctx context.Context, c *config, out io.Writer) error {
+	c.init(os.Args)
+	log.SetOutput(out)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-time.Tick(c.tick):
+			err := generatePayload(c, fileList)
+			if err != nil {
+				fmt.Println("Error:")
+				fmt.Println(err)
+				return err
+			}
+			for _, entry := range fileList {
+				if entry.Binarydata != nil {
+					err = call(c.statshosturl, "POST", entry)
+					if err != nil {
+						fmt.Println("error: ")
+						fmt.Println(err)
+						return err
+					}
+					fmt.Printf("%s:  - %s\n", entry.Filename, strconv.Itoa(entry.Timestamp))
+					entry.Binarydata = nil
+				}
+			}
+		}
+	}
+}
+
+// Cretes payloads for all files in a struct slice.
+func generatePayload(c *config, payload []payloadEntry) error {
 
 	var b []byte
 	var found bool
 
-	files, err := ioutil.ReadDir(directoryPath)
+	files, err := ioutil.ReadDir(c.directory)
 	if err != nil {
+		fmt.Println("Error:")
+		fmt.Println(err)
 		log.Fatal(err)
+		return err
 	}
-	var decgrep = findDecgrep()
 
 	for _, file := range files {
 		found = false
-		var filePath = directoryPath + file.Name()
+		var filePath = c.directory + file.Name()
 
-		for _, v := range fileList {
+		for i, _ := range fileList {
 			var filename = file.Name()
-			if v.Filename == filename {
+			if fileList[i].Filename == filename {
 				found = true
-				v.Binarydata, err = exec.Command(decgrep, "-f", "4", "-s", strconv.Itoa(v.Timestamp), filePath).Output()
+				fileList[i].Binarydata, err = exec.Command(c.binarytocall, "-f", "4", "-s", strconv.Itoa(fileList[i].Timestamp), filePath).Output() //adding timestamp to call, with flag -s
 				if err != nil {
+					fmt.Println("Error:")
 					fmt.Println(err.Error())
+					return err
 				}
-				v.Timestamp = int(time.Now().UnixMilli())
-				fmt.Println(v.Timestamp)
-
+				fileList[i].Timestamp = int(time.Now().UnixMilli())
 			}
 		}
 
 		if !found {
-			b, err = exec.Command(decgrep, "-f", "4", filePath).Output()
+			b, err = exec.Command(c.binarytocall, "-f", "4", filePath).Output()
 			if err != nil {
+				fmt.Println("Error:")
 				fmt.Println(err.Error())
+				return err
 			}
 			newEntry := payloadEntry{Filename: file.Name(), Timestamp: int(time.Now().UnixMilli()), Binarydata: b}
-			fmt.Println(time.Now().UnixMilli())
 			fileList = append(fileList, newEntry)
 		}
 
 	}
-
-	return fileList
+	return nil
 
 }
 
@@ -84,7 +184,6 @@ func call(urlPath, method string, payload payloadEntry) error {
 	client := &http.Client{
 		Timeout: time.Second * 10,
 	}
-
 	req, err := http.NewRequest(method, urlPath, bytes.NewReader(payload.Binarydata))
 	if err != nil {
 		fmt.Println("Error on generating request!")
@@ -97,31 +196,7 @@ func call(urlPath, method string, payload payloadEntry) error {
 	rsp, _ := client.Do(req)
 	if rsp.StatusCode != http.StatusOK {
 		log.Printf("Request failed with response code: %d", rsp.StatusCode)
+		return errors.New(string(rsp.StatusCode))
 	}
 	return nil
-}
-
-func findDecgrep() string {
-
-	return "/bin/decgrep"
-	// var absPath string
-
-	// fileErr := filepath.Walk("/", func(path string, info os.FileInfo, err error) error {
-	// 	// path is the absolute path.
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		return err
-	// 	}
-	// 	if info.Name() == "decgrep" {
-	// 		// if the filename is found return the absolute path of the file.
-	// 		absPath = path
-	// 	}
-
-	// 	return nil
-	// })
-	// if fileErr != nil {
-	// 	fmt.Println(fileErr)
-	// }
-	// // fmt.Println(absPath)
-	// return absPath
 }
