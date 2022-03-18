b76e03f62eab060c12df08cb5233c299b1883a85
Mike Brennan <mike@livelyvideo.tv>
2022-03-18 14:57:34 -0500

    Refactor server finished.  Running for some time to determine issues

--- server.go
+++ server.go
@@ -11,41 +11,17 @@ import (
 	"strings"
 	"syscall"
 	"errors"
-	"time"
-	"strconv"
-	"sort"
-	"bytes"
-	// "encoding/base64"
-
 )
-
-type event struct {
-	binaryData   []byte
-	filename   	 string
-	timestamp	 int
-
-}
-
-
-var entryList []event
-
 
 const file_mode = 0644
 
 func dataIn(w http.ResponseWriter, req *http.Request) {
-
-	var duplicate bool
-
-	directory := getenv("OUTPUT_DIRECTORY","/received")
-
 	if req.Method == "POST" {
-		duplicate = false
 		// Grab header data from request to create the right location and filename to append the incoming data.
-    	// TODO: handle missing fields, and check if the output directory exists
-		// fmt.Printf("%s:  - %s\n", req.Header["Filename"], req.Header["Timestamp"])
+    	    // TODO: handle missing fields, and check if the output directory exists
+		fmt.Printf("%s:  - %s\n", req.Header["Filename"], req.Header["Timestamp"])
 		justfile := strings.Join(req.Header["Filename"], "")
-	
-
+		directory := getenv("OUTPUT_DIRECTORY","/received") 
 
 		if !strings.HasSuffix(directory, "/") {
 			directory = directory + "/"
@@ -56,71 +32,27 @@ func dataIn(w http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			log.Fatal("Req reading data: ", err)
 		}
-		timestamp, err := strconv.Atoi(strings.Join(req.Header["Timestamp"],"") )
-		if err != nil {
-			log.Fatal("Error generating timestamp int", err)
-		}
-
-		for _,event := range entryList {
-			if (justfile == event.filename) && (timestamp == event.timestamp) {
-				duplicate = true
-				// fmt.Println("File found: " + event.filename)
-				// if  bytes.Compare(event.binaryData , buf) == 0 {
-				// 	duplicate = true
-				// 	fmt.Println(req.Header["Timestamp"])
-				// 	fmt.Println("Duplicate data from last receive! " + justfile)
-				// 	sEnc := base64.StdEncoding.EncodeToString([]byte(buf))
-				// 	fmt.Println("Base64 of request buffer:")
-				// 	fmt.Println(string(sEnc))
-				// 	fmt.Println("Base64 of saved data:")
-				// 	hEnc := base64.StdEncoding.EncodeToString([]byte(event.binaryData))
-				// 	fmt.Println(string(hEnc))		
-				// } 
-				// event.binaryData = buf
-			}
-			if  bytes.Compare(event.binaryData , buf) == 0 {
-				duplicate = true
-			}
-		}
-
-		if !duplicate {
-			newEntry := event{filename:justfile , binaryData: buf, timestamp: timestamp }
-			entryList = append(entryList, newEntry)
-		}
-		// if !duplicate {
-		// 	filename, err := buildFileName(directory,justfile)
-		// 	if err != nil {
-		// 		fmt.Println("Error with build name!")
-		// 		fmt.Println(err)
-		// 	}else{
-		// 		file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, file_mode)
-		// 		if err != nil {
-		// 			fmt.Println(err)
-		// 		}
-		// 		fmt.Printf("%s:  - %s -   Binarydata: %s\n", filename, req.Header["Timestamp"],  base64.StdEncoding.EncodeToString([]byte(buf)))
-		// 		_, err = file.Write(buf)
-		// 		if err != nil {
-		// 			fmt.Println("Error writing file")
-		// 			fmt.Println(err)
-		// 			log.Fatal(err)
-		// 		}
-		// 		if err := file.Close(); err != nil {
-		// 			fmt.Println("Error closing file: ")
-		// 			fmt.Println(err)
-		// 			log.Fatal(err)
-		// 		}
-		// 	}
-		// }
 		// func Split(s, sep string) []string
-	if len(entryList) >= 215 {
-		err := handleFiles()
-		if err != nil {
-			fmt.Println("Error with file handler!")
+		
+		filename, err := buildFileName(directory,justfile)
+        if err != nil {
+			fmt.Println("Error with build name!")
 			fmt.Println(err)
-			log.Fatal(err)
+		}else{
+			fmt.Println("Open file: " + filename)
+			file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, file_mode)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			_, err = file.Write(buf)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if err := file.Close(); err != nil {
+				log.Fatal(err)
+			}
 		}
-	}
-	
 
 	} else {
 		fmt.Println("Not a POST!")
@@ -145,7 +77,7 @@ func buildFileName(directory string, filename string) (string, error) {
 	}
 
 	path := directory + "/" + podName
-	// fmt.Println("Directory to build:  " + path)
+	fmt.Println("Directory to build:  " + path)
 	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
 		err := os.Mkdir(path, os.ModePerm)
 		if err != nil {
@@ -197,11 +129,9 @@ func main() {
 		}
 	}()
 
-	
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", dataIn)
 	log.Fatalln(http.ListenAndServe(":"+getenv("SERVER_PORT","80"), mux))
-
 
 }
 
@@ -213,113 +143,4 @@ func getenv(key, fallback string) string {
         return fallback
     }
     return value
-}
-
-
-func handleFiles() error {
-
-	directory := getenv("OUTPUT_DIRECTORY","/received") 
-	
-	sort.SliceStable(entryList, func(i, j int) bool { return entryList[i].filename < entryList[j].filename })
-	sort.SliceStable(entryList, func(i, j int) bool { return entryList[i].timestamp < entryList[j].timestamp })
-
-	// for i,entry := range entryList {
-	// 	for j,secEntry := range entryList {
-	// 		if i != j {
-	// 			if entry == secEntry {
-
-	// 			}
-
-
-	// 		}
-
-	uniqueList := unique(entryList)
-
-    for _,entry := range uniqueList {
-			
-		filename, err := buildFileName(directory,entry.filename)
-		if err != nil {
-			fmt.Println("Error with build name!")
-			fmt.Println(err)
-			return err
-		}else{
-			file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, file_mode)
-			if err != nil {
-				fmt.Println(err)
-				return err
-			}
-			fmt.Printf("%s:  - %s \n", filename, strconv.Itoa(entry.timestamp))
-			_, err = file.Write(entry.binaryData)
-			if err != nil {
-				fmt.Println("Error writing file")
-				fmt.Println(err)
-				log.Fatal(err)
-				return err
-			}
-			if err := file.Close(); err != nil {
-				fmt.Println("Error closing file: ")
-				fmt.Println(err)
-				log.Fatal(err)
-				return err
-			}
-		}
-	}
-	fmt.Println("Emptying list!")
-	entryList = nil
-				// if !duplicate {
-		// 	filename, err := buildFileName(directory,justfile)
-		// 	if err != nil {
-		// 		fmt.Println("Error with build name!")
-		// 		fmt.Println(err)
-		// 	}else{
-		// 		file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, file_mode)
-		// 		if err != nil {
-		// 			fmt.Println(err)
-		// 		}
-		// 		fmt.Printf("%s:  - %s -   Binarydata: %s\n", filename, req.Header["Timestamp"],  base64.StdEncoding.EncodeToString([]byte(buf)))
-		// 		_, err = file.Write(buf)
-		// 		if err != nil {
-		// 			fmt.Println("Error writing file")
-		// 			fmt.Println(err)
-		// 			log.Fatal(err)
-		// 		}
-		// 		if err := file.Close(); err != nil {
-		// 			fmt.Println("Error closing file: ")
-		// 			fmt.Println(err)
-		// 			log.Fatal(err)
-		// 		}
-		// 	}
-		// 
-	
-	return nil
-}
-
-func delaySecond(n time.Duration) {
-	time.Sleep(n * time.Second)
-}
-func entriesEqual(first, second event) bool {
-	if first.filename == second.filename && first.timestamp == second.timestamp && (bytes.Compare(first.binaryData , second.binaryData) == 0) {
-		return true
-	}else{
-		return false
-	}
-}
-
-func unique(sample []event) []event {
-	var unique []event
-	var found bool
-	
-	for _,firstEntry := range sample {
-		found = false
-		for _,secondEntry := range unique {
-			if firstEntry.filename == secondEntry.filename && firstEntry.timestamp == secondEntry.timestamp && (bytes.Compare(firstEntry.binaryData , secondEntry.binaryData) == 0) {
-				found = true
-				fmt.Printf("Already found: %s - %s",firstEntry.filename,firstEntry.timestamp)
-			} 
-		}
-		if !found {
-			unique = append(unique, firstEntry)
-		}
-	}
-	return unique
 }
